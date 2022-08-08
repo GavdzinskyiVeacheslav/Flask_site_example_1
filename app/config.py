9d2d24608be77d22555c8626f023fc9a20b575c8
Gavdzynskyi Viacheslav <62297406+GavdzinskyiVeacheslav@users.noreply.github.com>
2022-08-08 15:34:11 +0300

    Add files via upload

---
+++ app/config.py
@@ -0,0 +1,4 @@
+class Configuration(object):
+	DEBUG = True
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
+	SQLALCHEMY_DATABASE_URI = 'mysql://root:root@localhost/test1'
---
+++ app/main.py
@@ -0,0 +1,10 @@
+from app import app
+from app import db
+import view
+
+if __name__ == '__main__':
+	app.run(debug=True)
+
+
+
+
